2660e09fba504a19501276672dd523b9bcf15519
alonso-kuri <alonso.garcia@tolucafc.com>
2025-07-18 14:17:08 -0600

    Update interacciones.js

--- js/interacciones.js
+++ js/interacciones.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const pdfButton = document.querySelector('.pdf-button');
   if (pdfButton) {
     pdfButton.addEventListener('click', () => {
-      alert('¡Estás a punto de abrir el menú PDF!');
+      window.open('ruta/del/menu.pdf', '_blank');
     });
   }
 });
