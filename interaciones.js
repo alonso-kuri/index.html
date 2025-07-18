5fc47270a32f0b9581d5547028ae1ddaf12f0f83
alonso-kuri <alonso.garcia@tolucafc.com>
2025-07-18 14:12:03 -0600

    Delete interaciones.js

--- interaciones.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const pdfButton = document.querySelector('.pdf-button');
-  if (pdfButton) {
-    pdfButton.addEventListener('click', () => {
-      alert('¡Estás a punto de abrir el menú PDF!');
-    });
-  }
-});
